b5b58280efd4a092184a606c7a4b55d979f40d14
arifan <arifmuhammad.idn@gmail.com>
2022-06-09 17:08:39 +0700

    change head icon

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <script setup lang="ts">
   import NavbarVue from '@/components/Navbar.vue';
   import { useHead } from '@vueuse/head';
+  import logo1NoBg from '@/assets/dafara/logo1_nobg.png';
 
   useHead({
+    link: [
+      { rel: 'icon', href: logo1NoBg },
+    ],
     title: 'Dafara Clothings',
     meta: [
       {
